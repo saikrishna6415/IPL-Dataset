087b7e74a0afa86b563dad01e3d48faa01cb9fb3
saikrishna <saikrishnakotagiri16@gmail.com>
2020-02-25 14:30:23 +0530

    refactore code

--- src/server/index.js
+++ src/server/index.js
@@ -4,53 +4,58 @@ var matches=require('../data/matches');
 var deliveries =    require ('../data/deliveries');
 
 
-
-var numberOfMatchesPlayedPerYear = results.numberOfMatchesPlayedPerYear(matches)
-// console.log(numberOfMatchesPlayedPerYear)
-// var numberOfMatchesPlayedPerYearJSON = JSON.stringify(numberOfMatchesPlayedPerYear); 
-// fs.writeFile('../output/numberOfMatchesPlayedPerYear.JSON',numberOfMatchesPlayedPerYearJSON,function(error){              //write output for numberOfMatchesPlayedPerYear
-//     if(error){
-//         throw error
-//     };
-//     console.log("numberOfMatchesPlayedPerYear.JSON file saved successfully!");
-// });
-
-
-
-var numberOfmatchesWonPerTeamPerYear = results.numberOfmatchesWonPerTeamPerYear(matches)
-// console.log(numberOfMatchesPlayedPerYearJSON)
-// var numberOfmatchesWonPerTeamPerYearJSON = JSON.stringify(numberOfmatchesWonPerTeamPerYear); 
-// fs.writeFile('../output/numberOfmatchesWonPerTeamPerYear.JSON',numberOfmatchesWonPerTeamPerYearJSON,function(error){              //write output for numberOfmatchesWonPerTeamPerYear
-//     if(error){
-//         throw error
-//     };
-//     console.log("numberOfmatchesWonPerTeamPerYear.JSON file saved successfully!");
-// });
+const csv=require('csvtojson')
+csv().fromFile('../data/matches.csv').then((matches)=>{
+csv().fromFile('../data/deliveries.csv').then((deliveries)=>{
+    var numberOfMatchesPlayedPerYear = results.numberOfMatchesPlayedPerYear(matches)
+    // console.log(numberOfMatchesPlayedPerYear)
+    var numberOfMatchesPlayedPerYearJSON = JSON.stringify(numberOfMatchesPlayedPerYear); 
+    fs.writeFile('../output/numberOfMatchesPlayedPerYear.JSON',numberOfMatchesPlayedPerYearJSON,function(error){
+        if(error){
+            throw error
+        };
+        console.log("numberOfMatchesPlayedPerYear.JSON file saved successfully!");
+    });
+   //write output for numberOfMatchesPlayedPerYear
 
 
 
-
-var extraRunsIn2016 = results.extraRunsIn2016(matches,deliveries)
-// console.log(extraRunsIn2016)
-// var extraRunsIn2016JSON = JSON.stringify(extraRunsIn2016); 
-// fs.writeFile('../output/extraRunsIn2016.JSON',extraRunsIn2016JSON,function(error){              //write output for extraRunsIn2016
-//     if(error){
-//         throw error
-//     };
-//     console.log("extraRunsIn2016.JSON file saved successfully!");
-// });
-
+    var numberOfmatchesWonPerTeamPerYear = results.numberOfmatchesWonPerTeamPerYear(matches)
+    // console.log(numberOfMatchesPlayedPerYearJSON)
+    var numberOfmatchesWonPerTeamPerYearJSON = JSON.stringify(numberOfmatchesWonPerTeamPerYear); 
+    fs.writeFile('../output/numberOfmatchesWonPerTeamPerYear.JSON',numberOfmatchesWonPerTeamPerYearJSON,function(error){
+        if(error){
+            throw error
+        };
+        console.log("numberOfmatchesWonPerTeamPerYear.JSON file saved successfully!");
+    }); // writes a json output for numberOfmatchesWonPerTeamPerYear
 
 
-var EconomicalBowlersForYear = results.economicalBowlers(matches,deliveries)
-console.log(EconomicalBowlersForYear)
-// var EconomicalBowlersForYearJSON = JSON.stringify(EconomicalBowlersForYear); 
-// fs.writeFile('../output/EconomicalBowlersForYear.JSON',EconomicalBowlersForYearJSON,function(error){              //write output for EconomicalBowlersForYear
-//     if(error){
-//         throw error
-//     };
-//     console.log("EconomicalBowlersForYear.JSON file saved successfully!");
-// });
+
+    var extraRunsIn2016 = results.extraRunsIn2016(matches,deliveries)
+    // console.log(extraRunsIn2016)
+    var extraRunsIn2016JSON = JSON.stringify(extraRunsIn2016); 
+    fs.writeFile('../output/extraRunsIn2016.JSON',extraRunsIn2016JSON,function(error){              //write output for extraRunsIn2016
+        if(error){
+            throw error
+        };
+        console.log("extraRunsIn2016.JSON file saved successfully!");
+    });
+
+    var EconomicalBowlersForYear = results.economicalBowlers(matches,deliveries)
+    // console.log(EconomicalBowlersForYear)
+    var EconomicalBowlersForYearJSON = JSON.stringify(EconomicalBowlersForYear); 
+    fs.writeFile('../output/EconomicalBowlersForYear.JSON',EconomicalBowlersForYearJSON,function(error){              //write output for EconomicalBowlersForYear
+        if(error){
+            throw error
+        };
+        console.log("EconomicalBowlersForYear.JSON file saved successfully!");
+    });
+
+        })
+})
+
+
 
 
 
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,14 +1,14 @@
 const numberOfMatchesPlayedPerYear =(matches) => {
-    return matches.reduce((acc, match) => {
-        // console.log(acc)                                                           //1
-        if(acc[match.season] ) { 
-             acc[match.season]++   
-            } 
-         else {
-            acc[match.season] =1;
-            }
-        return acc;
-    },{})
+  return matches.reduce((matchesplayed, match) => {
+      // console.log(matchesplayed)                                                           //1
+      if(matchesplayed[match.season] ) { 
+           matchesplayed[match.season]++   //count numbers of matches played per year
+          } 
+       else {
+          matchesplayed[match.season] =1;
+          }
+      return matchesplayed;
+  },{})
 }
 
 // console.log(numberOfMatchesPlayedPerYear(matches))
@@ -18,35 +18,35 @@ const numberOfMatchesPlayedPerYear =(matches) => {
 
 
 const numberOfmatchesWonPerTeamPerYear = (matches) => {
-    return matches.reduce((acc,match) => {
-        if (! acc[match.season]) {
-            acc[match.season] = {}
-        }
-        if (acc[match.season][match.winner]) {
-            acc[match.season][match.winner] ++                   //2nd answer method (i)
-        }
-        else {
-            acc[match.season][match.winner] =1
-        }
-        return acc;
-    },{})
+  return matches.reduce((matcheswonperyear,match) => {
+      if (! matcheswonperyear[match.season]) {
+          matcheswonperyear[match.season] = {}                          
+      }   
+      if (matcheswonperyear[match.season][match.winner]) {        //count number of matches played per team per year
+          matcheswonperyear[match.season][match.winner] ++                   // method (i)
+      }
+      else {
+          matcheswonperyear[match.season][match.winner] =1        
+      }
+      return matcheswonperyear;
+  },{})
 }
 // console.log(numberOfmatchesWonPerTeamPerYear(matches))
 
 
 
 // const numberOfmatchesWonPerTeamPerYear = (matches) => {
-//     return matches.reduce((acc,match) => {
-//         if (! acc[match.winner]) {
-//             acc[match.winner] = {}
+//     return matches.reduce((matcheswonperyear,match) => {
+//         if (! matcheswonperyear[match.winner]) {
+//             matcheswonperyear[match.winner] = {}
 //         }
-//         if (acc[match.winner][match.season]) {
-//             acc[match.winner][match.season] ++                   //2nd answer method (ii)
+//         if (matcheswonperyear[match.winner][match.season]) {
+//             matcheswonperyear[match.winner][match.season] ++                   //method (ii)
 //         }
 //         else{
-//             acc[match.winner][match.season] =1
+//             matcheswonperyear[match.winner][match.season] =1
 //         }
-//         return acc;
+//         return matcheswonperyear;
 //     },{})
 
 // }
@@ -55,87 +55,89 @@ const numberOfmatchesWonPerTeamPerYear = (matches) => {
 
 
 const extraRunsIn2016 = (matches,deliveries) => {
-var matches2016 =[ ]
-for (let i = 0; i< matches.length; i++) {
-    if (matches[i]['season'] === '2016') {
-          matches2016.push(parseInt(matches[i]['id']))
+  var matches2016 =[ ]
+    matches.filter(match =>{
+      if (match.season === '2016'){                       //filtered 2016 matches
+        matches2016.push(parseInt(match.id))
       }
-  }
-    return deliveries.reduce((acc, delivery) => {
-        if ((delivery.match_id >= matches2016[0] && delivery.match_id <= matches2016[matches2016.length-1]) ) {
-          if (acc[delivery.bowling_team]) {   
-            acc[delivery.bowling_team] = acc[delivery.bowling_team] + parseInt(delivery.extra_runs)
-          }
-          else {
-            acc[delivery.bowling_team] =parseInt( delivery.extra_runs)
-          }
+    })
+  return deliveries.reduce((extraruns, delivery) => {
+    if ((delivery.match_id >= matches2016[0] && delivery.match_id <= matches2016[matches2016.length-1]) ) {
+        if (extraruns[delivery.bowling_team]) {   
+          extraruns[delivery.bowling_team] = extraruns[delivery.bowling_team] + parseInt(delivery.extra_runs)         //count numbers of extra runs given by bowling team
         }
-        return acc;
-    },{})
-  }
+        else {
+          extraruns[delivery.bowling_team] =parseInt( delivery.extra_runs)
+        }
+      }
+      return extraruns;
+  },{})
+}
 
 
-  const economicalBowlers = (matches,deliveries) => {
-    var matches2015 =[ ]
-    for (let i = 0; i< matches.length; i++) {
-        if (matches[i]['season'] === '2015') {
-            matches2015.push(parseInt(matches[i]['id']))
+const economicalBowlers = (matches,deliveries) => {
+  var matches2015 =[ ]
+    matches.filter(match =>{  
+      if (match.season === '2015'){                       //filtered matches played in 2015
+        matches2015.push(parseInt(match.id))
+      } 
+    })
+  var balls = deliveries.reduce((ballsbowled, delivery) => {
+      if (parseInt(delivery.match_id) >=matches2015[0] && parseInt(delivery.match_id) <= matches2015[matches2015.length-1]) { 
+      if (delivery.noball_runs === "0" && delivery.wide_runs==="0"){
+        if ( ballsbowled[delivery.bowler]) {
+          ballsbowled[delivery.bowler]++                    //count numbers of balls bowled by particualar bowler
         }
+        else {
+          ballsbowled[delivery.bowler] = 1
+        }
+      }
+      }
+      return ballsbowled
+    }, {})
+  
+  
+  
+    var runs = deliveries.reduce((runsgiven, delivery) => {
+      if (parseInt(delivery.match_id) >=matches2015[0] && parseInt(delivery.match_id) <= matches2015[matches2015.length-1]) {                                       // Function Will return total number of run given by bowler   
+      if (runsgiven[delivery.bowler]) {
+          runsgiven[delivery.bowler] = runsgiven[delivery.bowler] + parseInt(delivery.total_runs)         //count numbers of runs given by bowler
+        }
+        else {
+          runsgiven[delivery.bowler] = parseInt(delivery.total_runs)
+        }
+      }
+      return runsgiven
+    }, {})
+  var run = Object.values(runs)                     //runs given by bowler
+  var ball = Object.values(balls)                   //balls bowled by bowler
+  var player = Object.keys(balls)
+  var economy = [];
+  var sortedEconomy = [];
+  for (let i = 0; i < run.length; i++) {  
+      economy.push((run[i] / ball[i]) * 6)        //calculate economy 
     }
+    for (let j = 0; j < economy.length; j++) {
+      sortedEconomy.push(economy[j])                        
+    }
+    sortedEconomy.sort(function (a, b) {
+      return a - b                              //sorting economy
+    })
+    var result = {}
+    for (var j = 0; j < 10; j++) {
+      var top10 = economy.indexOf(sortedEconomy[j]) 
+      result[player[top10]] = sortedEconomy[j]         
+    }
+    return result;
 
-    var balls = deliveries.reduce((acc, delivery) => {
-        if (parseInt(delivery.match_id) >=matches2015[0] && parseInt(delivery.match_id) <= matches2015[matches2015.length-1]) { 
-         if (delivery.noball_runs === "0"){
-          if ( acc[delivery.bowler]) {
-            acc[delivery.bowler]++
-          }
-          else {
-            acc[delivery.bowler] = 1
-          }
-        }
-        }
-        return acc
-      }, {})
-    
-    
-    
-      var runs = deliveries.reduce((acc, delivery) => {
-        if (parseInt(delivery.match_id) >=matches2015[0] && parseInt(delivery.match_id) <= matches2015[matches2015.length-1]) {                                       // Function Will return total number of run given by bowler 
-          if (acc[delivery.bowler]) {
-            acc[delivery.bowler] = acc[delivery.bowler] + parseInt(delivery.total_runs)
-          }
-          else {
-            acc[delivery.bowler] = parseInt(delivery.total_runs)
-          }
-        }
-        return acc
-      }, {})
-    var run = Object.values(runs)
-    var ball = Object.values(balls)
-    var player = Object.keys(balls)
-    var economy = [];
-    var sortedEconomy = [];
-    for (let i = 0; i < run.length; i++) {  
-        economy.push((run[i] / ball[i]) * 6)
-      }
-      for (let j = 0; j < economy.length; j++) {
-        sortedEconomy.push(economy[j])                        
-      }
-      sortedEconomy.sort(function (a, b) {
-        return a - b
-      })
-      var result = {}
-      for (var j = 0; j < 10; j++) {
-        var top10 = economy.indexOf(sortedEconomy[j]) 
-        result[player[top10]] = sortedEconomy[j]
-      }
-      return result         ;
+}
 
-  }
+module.exports = {
+numberOfMatchesPlayedPerYear,
+numberOfmatchesWonPerTeamPerYear, extraRunsIn2016, economicalBowlers
+}
 
-
-
-module.exports.numberOfMatchesPlayedPerYear = numberOfMatchesPlayedPerYear;
-module.exports.numberOfmatchesWonPerTeamPerYear =numberOfmatchesWonPerTeamPerYear;
-module.exports.extraRunsIn2016 = extraRunsIn2016;
-module.exports.economicalBowlers = economicalBowlers;
+// module.exports.numberOfMatchesPlayedPerYear = numberOfMatchesPlayedPerYear;
+// module.exports.numberOfmatchesWonPerTeamPerYear =numberOfmatchesWonPerTeamPerYear;
+// module.exports.extraRunsIn2016 = extraRunsIn2016;
+// module.exports.economicalBowlers = economicalBowlers;
